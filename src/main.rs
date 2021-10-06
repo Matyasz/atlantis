6f6be16f7567324a7aeca84d3e23fd15e7b69392
Taylor Matyasz <tjmatyasz@gmail.com>
2021-10-06 01:16:03 -0700

    Successfully reads and deserializes JSON data passed from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{self, BufRead};
+use serde_json::{self, Value};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let data: Value = serde_json::from_str(&line.unwrap()).unwrap();
+
+        println!("{}", data["1"]);
+    }
 }
